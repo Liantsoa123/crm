e5b423015086390d23ecd90a0eba7de3af695df0
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-25 23:00:01 +0300

    feat: Add data generation form and functionality for managers.

--- src/main/java/site/easy/to/build/crm/controller/SystemController.java
+++ src/main/java/site/easy/to/build/crm/controller/SystemController.java
@@ -8,18 +8,23 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
+import site.easy.to.build.crm.service.generate.GenerateData;
 import site.easy.to.build.crm.service.system.SystemService;
 import site.easy.to.build.crm.util.AuthorizationUtil;
+
+import java.sql.SQLDataException;
 
 @Controller
 @RequestMapping("/manager/system")
 public class SystemController {
 
     private final SystemService systemService;
+    private final GenerateData generateData;
 
     @Autowired
-    public SystemController(SystemService systemService) {
+    public SystemController(SystemService systemService, GenerateData generateData) {
         this.systemService = systemService;
+        this.generateData = generateData;
     }
 
     @GetMapping("reset/confirm")
@@ -52,4 +57,29 @@ public class SystemController {
         }
         return "reset/reset";
     }
+
+    @GetMapping("generate/form")
+    public String showGenerateForm(Authentication authentication, Model model) {
+        // Check if user has manager role
+        if (!AuthorizationUtil.hasRole(authentication, "ROLE_MANAGER")) {
+            return "error/access-denied";
+        }
+        return "generate/generateData";
+    }
+
+    @PostMapping("generate")
+    public String generateData(Authentication authenticationa,
+                               @RequestParam("customerCount") int customerCount,
+                               @RequestParam("budgetPerCustomer") int budgetPerCustomer,
+                               @RequestParam("ticketPerCustomer") int ticketPerCustomer,
+                               @RequestParam("leadPerCustomer") int leadPerCustomer,Model model) throws SQLDataException {
+        // Check if user has manager role
+        if (!AuthorizationUtil.hasRole(authenticationa, "ROLE_MANAGER")){
+            return "error/access-denied";
+        }
+        generateData.generateAndSaveRandomData(customerCount, budgetPerCustomer, ticketPerCustomer, leadPerCustomer);
+        model.addAttribute("successMessage", "Les données ont été générées avec succès.");
+        return "generate/generateData";
+    }
+
 }
