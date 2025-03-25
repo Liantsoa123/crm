898acd1e5b8b4c44a52be425d17c383066fb4364
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-25 18:51:53 +0300

    feat: Enhance ImportCsvController to save all data from CSV files with error handling.

--- src/main/java/site/easy/to/build/crm/controller/ImportCsvController.java
+++ src/main/java/site/easy/to/build/crm/controller/ImportCsvController.java
@@ -3,6 +3,8 @@ package site.easy.to.build.crm.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,16 @@ import org.springframework.web.multipart.MultipartFile;
 import site.easy.to.build.crm.dto.BudgetCsvDTO;
 import site.easy.to.build.crm.dto.CustomerCsvDTO;
 import site.easy.to.build.crm.dto.TicketLeadCsvDTO;
+import site.easy.to.build.crm.entity.Budget;
+import site.easy.to.build.crm.entity.Customer;
+import site.easy.to.build.crm.entity.Ticket;
+import site.easy.to.build.crm.entity.User;
+import site.easy.to.build.crm.service.budget.BudgetCsvDTOServiceImpl;
+import site.easy.to.build.crm.service.budget.BudgetServiceImpl;
 import site.easy.to.build.crm.service.csv.CsvImportService;
+import site.easy.to.build.crm.service.customer.CustomerCsvDTOServiceImpl;
+import site.easy.to.build.crm.service.customer.CustomerServiceImpl;
+import site.easy.to.build.crm.service.user.UserServiceImpl;
 import site.easy.to.build.crm.util.AuthorizationUtil;
 
 import java.io.IOException;
@@ -24,6 +35,11 @@ import java.util.List;
 public class ImportCsvController {
 
     private final CsvImportService csvImportService;
+    private final CustomerCsvDTOServiceImpl customerCsvDTOServiceImpl;
+    private final UserServiceImpl userServiceImpl;
+    private final CustomerServiceImpl customerServiceImpl;
+    private  final BudgetCsvDTOServiceImpl budgetCsvDTOServiceImpl;
+    private final BudgetServiceImpl budgetServiceImpl;
 
     @GetMapping("/form")
     public String showImportForm(Authentication authentication) {
@@ -68,10 +84,20 @@ public class ImportCsvController {
                 return "import/form";
             }
 
+            // Save all data
+            StringBuilder errorMessage = new StringBuilder();
+            csvImportService.saveALl(customers, budgets, ticketLeads, errorMessage );
+
+            if (!errorMessage.isEmpty()) {
+                model.addAttribute("errorMessage", errorMessage.toString());
+                return "import/form";
+            }
+
             // Add success message
             model.addAttribute("successMessage", "CSV files imported successfully!");
-        } catch (IOException e) {
-            model.addAttribute("errorMessage", "Error reading CSV files: " + e.getMessage());
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error reading CSV files: " + e.getMessage() +". Please check the files and try again.");
+            e.printStackTrace();
         }
 
         return "import/form";
